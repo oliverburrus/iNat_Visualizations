c519eaa648643d08164b07f3d65e886d856b803b
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2020-07-05 21:26:27 -0500

    Add files via upload

---
+++ Tables/iNat_obs.R
@@ -0,0 +1,38 @@
+#This code allows you to find out with species you have observed on iNat.
+
+#Replace the following text with your iNat username
+user <- "your_username"
+
+#Load required packages
+library(httr)
+library(rlist)
+library(reactable)
+library(dplyr)
+
+#API set-up
+x <- 1
+resp <- GET(paste("https://api.inaturalist.org/v1/observations/species_counts?user_id=", user, "&page=", as.character(x), "&hrank=species", sep = ""))
+parsed <- content(resp, as = "parsed")
+
+#Retreving data from the API
+while(x < (parsed$total_results/500)+1){
+  resp <- GET(paste("https://api.inaturalist.org/v1/observations/species_counts?user_id=", user, "&page=", as.character(x), "&hrank=species", sep = ""))
+  parsed <- content(resp, as = "parsed")
+  modJSON <- parsed$results %>%
+    list.select(taxon$name, count, taxon$observations_count, taxon$iconic_taxon_name)
+  if(x == 1){
+    data <- list.stack(modJSON)
+  }
+  if(x > 1){
+    dataz <- list.stack(modJSON)
+    data <- rbind(data, dataz)
+  }
+  x <- x+1
+}
+
+data <- data.frame(data$V1, data$count, data$V2, data$V3)
+names(data) <- c("Species", "Your Obs", "Total Obs", "Iconic Taxon")
+
+#Build the table
+a <- reactable(data, striped = TRUE, showPageSizeOptions = TRUE, pageSizeOptions = c(5, 10, 20, 100), filterable = TRUE)
+a
